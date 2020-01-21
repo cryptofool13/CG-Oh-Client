5f2644e764b5dd032804ddceb6dbd5a37038aaf4
michaelcurry95 <fender18295@gmail.com>
2020-01-20 20:35:29 -0500

    quagga scans

--- src/components/Scanner.svelte
+++ src/components/Scanner.svelte
@@ -1,11 +1,59 @@
 <script>
-import Quagga from 'quagga'
+  import { onMount } from "svelte";
+  import Quagga from "quagga";
 
- 
+  let scans = {}
+
+onMount(() => {
+    Quagga.init(
+      {
+        inputStream: {
+          type: "LiveStream",
+          constraints: {
+            width: 400,
+            height: 400
+          }
+        },
+        //locate: true,
+        decoder: {
+          readers: [{ format: "upc_reader", config: {} }]
+        }
+      },
+      e => {
+        if (e) console.log(e);
+        // Quagga.start();
+      }
+    );
+    Quagga.onDetected(data => {
+      let code = data.codeResult.code
+      if(scans[code]) {
+        scans[code]+=1
+      }else{
+      scans[code] = 1
+      }
+      console.log(scans);
+    });
+
+    return () => {
+      Quagga.stop();
+    };
+  });
+
+  function scan() {
+    Quagga.start()
+    setTimeout(() => {
+      Quagga.pause()
+    }, 1500)
+  }
 </script>
 
 <style>
-
+  #interactive {
+    width: 200px;
+    height: 200px;
+  }
 </style>
+
+<button on:click={scan}>Scan</button>
 
 <div id="interactive" class="viewport" />
